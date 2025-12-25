0029e6ed0f84a4ddbe7407b27d9f2ceb1427bd10
auhe <adrian.uhe@uni-bielefeld.de>
2025-12-25 20:07:32 +0100

    python linting and formatting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
 import csv
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
-from io import BytesIO, StringIO
-import zipfile
-from app.services import editExcel
 import os
-import shutil
+import zipfile
 from datetime import datetime
+from io import BytesIO, StringIO
+
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+
+from app.services import editExcel
 
 today = datetime.today().date()
 app = FastAPI()
@@ -16,7 +17,7 @@ origins = [
     "http://localhost:4321",
     "http://127.0.0.1:4321",
     "http://localhost:3000",
-    "http://localhost:5173"
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
@@ -25,25 +26,30 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["Content-Disposition"]
+    expose_headers=["Content-Disposition"],
 )
+
 
 @app.get("/")
 async def root():
     return {"message": "FastAPI mit CORS aktiviert!"}
 
+
 OUTPUT_FOLDER = "output"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
+
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...), option: str = Form(...)):
-    
+
     content = await file.read()
-    
+
     match option:
         case "jf_to_awin":
             print(option)
-            accepted_data, declined_data, amended_data = editExcel.process_csv_jf_to_awin(content=content)
+            accepted_data, declined_data, amended_data = (
+                editExcel.process_csv_jf_to_awin(content=content)
+            )
 
             def csv_to_str(data):
                 buffer = StringIO()
@@ -52,10 +58,21 @@ async def upload_file(file: UploadFile = File(...), option: str = Form(...)):
                 return buffer.getvalue()
 
             zip_buffer = BytesIO()
-            with zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED) as zip_file:
-                zip_file.writestr(f"accepted_{today.strftime('%Y%m%d')}_{file.filename}.csv", csv_to_str(accepted_data))
-                zip_file.writestr(f"declined_{today.strftime('%Y%m%d')}_{file.filename}.csv", csv_to_str(declined_data))
-                zip_file.writestr(f"amended_{today.strftime('%Y%m%d')}_{file.filename}.csv", csv_to_str(amended_data))
+            with zipfile.ZipFile(
+                zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED
+            ) as zip_file:
+                zip_file.writestr(
+                    f"accepted_{today.strftime('%Y%m%d')}_{file.filename}.csv",
+                    csv_to_str(accepted_data),
+                )
+                zip_file.writestr(
+                    f"declined_{today.strftime('%Y%m%d')}_{file.filename}.csv",
+                    csv_to_str(declined_data),
+                )
+                zip_file.writestr(
+                    f"amended_{today.strftime('%Y%m%d')}_{file.filename}.csv",
+                    csv_to_str(amended_data),
+                )
 
             zip_buffer.seek(0)
 
@@ -64,7 +81,7 @@ async def upload_file(file: UploadFile = File(...), option: str = Form(...)):
                 media_type="application/zip",
                 headers={
                     "Content-Disposition": f'attachment; filename="converted_{today.strftime("%Y%m%d")}.zip"'
-                }
+                },
             )
         case "jf_bonus":
             print(option)
@@ -73,7 +90,7 @@ async def upload_file(file: UploadFile = File(...), option: str = Form(...)):
             csv_buffer = StringIO()
             writer = csv.writer(csv_buffer, delimiter=";")
             writer.writerows(processed_data)
-            
+
             output = BytesIO()
             output.write(csv_buffer.getvalue().encode("utf-8"))
             output.seek(0)
@@ -83,9 +100,9 @@ async def upload_file(file: UploadFile = File(...), option: str = Form(...)):
             return StreamingResponse(
                 output,
                 media_type="text/csv",
-                headers={"Content-Disposition": f'attachment; filename="{filename}"'}
+                headers={"Content-Disposition": f'attachment; filename="{filename}"'},
             )
         case _:
             print("Nothing")
-            
+
     return {"message": "Nothing happened!"}
--- backend/app/services/editExcel.py
+++ backend/app/services/editExcel.py
@@ -1,24 +1,29 @@
 import csv
-from io import StringIO
 from datetime import datetime
+from io import StringIO
 
 month = datetime.now().month
 year = datetime.now().year
 
+
 def process_csv_jf_to_awin(content: bytes):
-    
-    declined = [
-        ["Order Reference", "Transaction Date", "Status", "Status Note"]
-    ]
+
+    declined = [["Order Reference", "Transaction Date", "Status", "Status Note"]]
 
     amended = [
-        ["Order Reference", "Transaction Date", "Status", "Status Note", "New Sale Price", "Commission Breakdown", "Currency"]
+        [
+            "Order Reference",
+            "Transaction Date",
+            "Status",
+            "Status Note",
+            "New Sale Price",
+            "Commission Breakdown",
+            "Currency",
+        ]
     ]
 
-    accepted = [
-        ["Order Reference", "Transaction Date", "Status"]
-    ]
-    
+    accepted = [["Order Reference", "Transaction Date", "Status"]]
+
     text_data = content.decode("utf-8")
     csv_file = StringIO(text_data)
     reader = csv.reader(csv_file, delimiter=";")
@@ -27,22 +32,38 @@ def process_csv_jf_to_awin(content: bytes):
     firstRow = True
 
     for row in reader:
-        if firstRow == True:
+        if firstRow:
             firstRow = False
             continue
         status = row[7]
         if status == "R":
-            if (row[11]==""):
+            if row[11] == "":
                 declinedRow = [row[0], row[4], "DECLINED", "Storniert"]
             else:
                 declinedRow = [row[0], row[4], "DECLINED", row[11]]
             declined.append(declinedRow)
             continue
         elif status == "T":
-            if (row[11]==""):
-                amendedRow = [row[0], row[4], "AMENDED", "Teilstorno", row[5][:-2], "", "EUR"]
+            if row[11] == "":
+                amendedRow = [
+                    row[0],
+                    row[4],
+                    "AMENDED",
+                    "Teilstorno",
+                    row[5][:-2],
+                    "",
+                    "EUR",
+                ]
             else:
-                amendedRow = [row[0], row[4], "AMENDED", row[11], row[5][:-2], "", "EUR"]
+                amendedRow = [
+                    row[0],
+                    row[4],
+                    "AMENDED",
+                    row[11],
+                    row[5][:-2],
+                    "",
+                    "EUR",
+                ]
             amended.append(amendedRow)
             continue
         elif status == "":
@@ -56,24 +77,31 @@ def process_csv_jf_to_awin(content: bytes):
 
     return processed_data
 
+
 def process_csv_jf_bonus(content: bytes):
     bonus = [
-        ["Advertiser_ID", "Publisher_ID", "Verguetungs_art", "Sale_betrag", "Bonus/Provisions_betrag", "Provisions_status", "Klick_Ref", "Auftrags_Nr"]
+        [
+            "Advertiser_ID",
+            "Publisher_ID",
+            "Verguetungs_art",
+            "Sale_betrag",
+            "Bonus/Provisions_betrag",
+            "Provisions_status",
+            "Klick_Ref",
+            "Auftrags_Nr",
+        ]
     ]
-    
-    bonus_map = {
-        
-    }
-    
+
+    bonus_map = {}
+
     text_data = content.decode("utf-8")
     csv_file = StringIO(text_data)
     reader = csv.reader(csv_file, delimiter=";")
 
-    processed_data = []
     firstRow = True
 
     for row in reader:
-        if firstRow == True:
+        if firstRow:
             firstRow = False
             continue
         publisher_id = row[1]
@@ -83,12 +111,31 @@ def process_csv_jf_bonus(content: bytes):
                 bonus_map[publisher_id] = 1
             else:
                 bonus_map[publisher_id] += 1
-        
+
     for map_publisher_id, map_bonus in bonus_map.items():
         if map_bonus >= 10:
-            bonus.append(["14899", map_publisher_id, "bonus", "0", bonus_to_cash(map_bonus), "confirmed", "", "BONUS_" + str(month) + "_" + str(year) + "_" + str(map_bonus) + "_" + str(map_publisher_id)])
-        
+            bonus.append(
+                [
+                    "14899",
+                    map_publisher_id,
+                    "bonus",
+                    "0",
+                    bonus_to_cash(map_bonus),
+                    "confirmed",
+                    "",
+                    "BONUS_"
+                    + str(month)
+                    + "_"
+                    + str(year)
+                    + "_"
+                    + str(map_bonus)
+                    + "_"
+                    + str(map_publisher_id),
+                ]
+            )
+
     return bonus
+
 
 def bonus_to_cash(bonus):
     if bonus >= 10 and not bonus >= 25:
